dd591502814f8b827f4913f50acfd3c02c2b9948
DylanMcBean <dylanmcbean2000@hotmail.com>
2023-01-15 23:34:13 +0000

    First Commit! :)

---
+++ README.md
@@ -0,0 +1,2 @@
+# learning_x86_assembly
+Just a few files of me learning some x86 assembly
---
+++ hello.asm
@@ -0,0 +1,22 @@
+; hello_world.asm
+;
+; Author: Dylan Mcbean
+; Date: 15/01/2023
+
+section .data
+    message: db "Hello, World!", 0xA
+    message_length equ $-message ; get length of string `message`
+
+section .text
+    global _start
+
+_start:
+    mov rax, 0x1            ; Write (int fd, cont void *buf, size_t count);
+    mov rdi, 0x1            ; fd
+    mov rsi, message        ; buf
+    mov rdx, message_length ; count
+    syscall                 ; call
+
+    mov rax, 60 ; Exit (int status)
+    mov rdi, 0  ; status
+    syscall     ; call
