4bb49c143af0715655b55d249a8a9fb5790f2abf
DylanMcBean <dylanmcbean2000@hotmail.com>
2023-01-16 05:29:43 +0000

    created "User Input" program

--- Hello World/main.asm
+++ Hello World/main.asm
@@ -5,14 +5,14 @@
 
 section .data
     message: db "Hello, World!", 0xA
-    message_length equ $-message ; get length of string `message`
+    message_length: equ $-message ; get length of string `message`
 
 section .text
-    global _start
+    global _start ; where is the start of the program
 
 _start:
-    mov rax, 0x1            ; Write (int fd, cont void *buf, size_t count);
-    mov rdi, 0x1            ; fd
+    mov rax, 1              ; Write (int fd, const void *buf, size_t count);
+    mov rdi, 1              ; fd - 1, stdout
     mov rsi, message        ; buf
     mov rdx, message_length ; count
     syscall                 ; call
